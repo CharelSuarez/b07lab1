64cbd30f8ed8d49a317c35b8acd05a6497ef660e
CharelSuarez <114007611+CharelSuarez@users.noreply.github.com>
2022-09-28 23:02:23 -0400

    Update Polynomial.java for lab 2, and update Driver.java (along with the addition of test_read.txt) with new test cases.

--- Driver.java
+++ Driver.java
@@ -1,17 +1,73 @@
+import java.io.File;
+import java.io.IOException;
+
 public class Driver {
 
 	public static void main(String [] args) {
 		Polynomial p = new Polynomial();
 		System.out.println(p.evaluate(3));
-		double [] c1 = {6,0,0,5};
-		Polynomial p1 = new Polynomial(c1);
-		double [] c2 = {0,-2,0,0,-9};
-		Polynomial p2 = new Polynomial(c2);
+		double [] c1 = {6,5};
+		int [] pow1 = {0,3};
+		Polynomial p1 = new Polynomial(c1, pow1);
+		double [] c2 = {0,-9};
+		int [] pow2 = {1, 4};
+		Polynomial p2 = new Polynomial(c2, pow2);
 		Polynomial s = p1.add(p2);
 		System.out.println("s(0.1) = " + s.evaluate(0.1));
-		if(s.hasRoot(1))
+		if (s.hasRoot(1)) {
 			System.out.println("1 is a root of s");
-		else
+		} else {
 			System.out.println("1 is not a root of s");
+		}
+
+		System.out.println("----- CHAREL'S TESTS -----");
+
+		try {
+			Polynomial test1 = new Polynomial(new File("test_read.txt"));
+			if ("5.0x0+1.0x1-2.0x2+3.0x5+3.25x22+69.0x57".equals(test1.toString())) {
+				System.out.println("Test #1: File read passed");
+			}
+		} catch (IOException ignored) {}
+
+		try {
+			Polynomial test2 = new Polynomial(new double[]{1.0, -55.0, 99.0, -120.0}, new int[]{0, 1, 60, 102});
+			test2.saveToFile("test_write.txt");
+			Polynomial test2_2 = new Polynomial(new File("test_write.txt"));
+			if ("1.0x0-55.0x1+99.0x60-120.0x102".equals(test2_2.toString())) {
+				System.out.println("Test #2: File write/read passed");
+			}
+		} catch (IOException ignored) {}
+
+		Polynomial test3 = new Polynomial(new double[]{1.0, -55.0, 99.0, -120.0}, new int[]{0, 1, 60, 102});
+		Polynomial test3_2 = new Polynomial(new double[]{23.0, 22.5, 56.75, 0.5}, new int[]{7, 2, 5, 200});
+		Polynomial test3_3 = test3.multiply(test3_2);
+		// Verified with symbollab
+		if ("22.5x2-1237.5x3+56.75x5-3121.25x6+23.0x7-1265.0x8+2227.5x62+5618.25x65+2277.0x67-2700.0x104-6810.0x107-2760.0x109+0.5x200-27.5x201+49.5x260-60.0x302".equals(test3_3.toString())) {
+			System.out.println("Test #3: Big multiply passed");
+		}
+
+		try {
+			test3_3.saveToFile("test_write.txt");
+			Polynomial test4 = new Polynomial(new File("test_write.txt"));
+			if ("22.5x2-1237.5x3+56.75x5-3121.25x6+23.0x7-1265.0x8+2227.5x62+5618.25x65+2277.0x67-2700.0x104-6810.0x107-2760.0x109+0.5x200-27.5x201+49.5x260-60.0x302".equals(test4.toString())) {
+				System.out.println("Test #4: Big read/write passed");
+			}
+		} catch (IOException ignored) {}
+
+
+		Polynomial test5 = new Polynomial(new double[]{1.0, -55.0, 99.0, -120.0}, new int[]{0, 1, 60, 102});
+		Polynomial test5_2 = new Polynomial(new double[0], new int[0]);
+		Polynomial test5_3 = test5.multiply(test5_2);
+		if ("0".equals(test5_3.toString())) {
+			System.out.println("Test #5: Zero multiply passed");
+		}
+
+		double test6 = test3_3.evaluate(1);
+		double test6_2 = test3_3.evaluate(1.0625);
+		// Verified with symbollab
+		if (test6 == -7706.25 && test6_2 + 5029964786.56046 < 0.0001) {
+			System.out.println("Test #6: Big evaluate passed");
+		}
+
 	}
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,30 +1,129 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Polynomial {
 
-    double[] coefficients;
+    private static final Pattern POLYNOMIAL_PATTERN = Pattern.compile("\\+?(?<coefficient>-?[\\d.]*)(?<power>x?\\d*)");
+
+    public double[] coefficients;
+    public int[] powers;
 
     public Polynomial() {
         this.coefficients = new double[0];
+        this.powers = new int[0];
     }
 
-    public Polynomial(double[] coefficients) {
+    public Polynomial(double[] coefficients, int[] powers) {
         this.coefficients = coefficients.clone();
+        this.powers = powers.clone();
+    }
+
+    public Polynomial(File file) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        String polynomialString = reader.readLine();
+        reader.close();
+
+
+        // Use regex to match the polynomials
+        Matcher matcher = Polynomial.POLYNOMIAL_PATTERN.matcher(polynomialString);
+        Map<Integer, Double> polynomials = new TreeMap<>();
+        while (matcher.find()) {
+            if ("".equals(matcher.group())) {
+                continue;
+            }
+            String powerString = matcher.group("power");
+            if ("".equals(powerString)) powerString = "0"; // ex. 5
+            else if ("x".equals(powerString)) powerString = "1"; // ex. 5x
+            else powerString = powerString.replace("x", "");
+            String coefficientString = matcher.group("coefficient");
+            if ("".equals(coefficientString)) coefficientString = "1"; // ex. x
+            else if ("-".equals(coefficientString)) coefficientString = "-1"; // ex. -x
+
+            Integer power = Integer.valueOf(powerString);
+            double coefficient = Double.parseDouble(coefficientString);
+            // Skip zeros
+            if (coefficient == 0) {
+                continue;
+            }
+            polynomials.put(power, polynomials.getOrDefault(power, 0.0) + coefficient);
+        }
+
+        // Put map back into arrays
+        this.coefficients = new double[polynomials.size()];
+        this.powers = new int[polynomials.size()];
+        int i = 0;
+        for (Map.Entry<Integer, Double> entry : polynomials.entrySet()) {
+            this.powers[i] = entry.getKey();
+            this.coefficients[i] = entry.getValue();
+            i++;
+        }
     }
 
     public Polynomial add(Polynomial other) {
-        double[] longest = this.coefficients.length >= other.coefficients.length ? this.coefficients : other.coefficients;
-        double[] shortest = longest == this.coefficients ? other.coefficients : this.coefficients;
-
-        double[] newCoefficients = longest.clone();
-        for (int i = 0; i < shortest.length; i++) {
-            newCoefficients[i] += shortest[i];
+        // Create map with this polynomial's values
+        Map<Integer, Double> polynomials = new TreeMap<>();
+        for (int i = 0; i < this.powers.length; i++) {
+            polynomials.put(this.powers[i], this.coefficients[i]);
         }
-        return new Polynomial(newCoefficients);
+        // Add other polynomial's values
+        for (int i = 0; i < other.powers.length; i++) {
+            polynomials.put(other.powers[i], polynomials.getOrDefault(other.powers[i], 0.0) + other.coefficients[i]);
+        }
+        // Remove 0s
+        polynomials.entrySet().removeIf(entry -> entry.getValue() == 0.0);
+        // Put back into arrays
+        return Polynomial.fromMap(polynomials);
+    }
+
+    public Polynomial multiply(Polynomial other) {
+        Map<Integer, Double> polynomials = new TreeMap<>();
+        for (int i = 0; i < this.powers.length; i++) {
+            int power = this.powers[i];
+            double coefficient = this.coefficients[i];
+            for (int j = 0; j < other.powers.length; j++) {
+                int power2 = other.powers[j];
+                double coefficient2 = other.coefficients[j];
+
+                polynomials.put(power + power2, polynomials.getOrDefault(power + power2, 0.0) + coefficient * coefficient2);
+            }
+        }
+
+        // Remove 0s
+        polynomials.entrySet().removeIf(entry -> entry.getValue() == 0.0);
+        // Put back into arrays
+        return Polynomial.fromMap(polynomials);
+    }
+
+    public void saveToFile(String fileName) throws IOException {
+        BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
+        writer.write(this.toString());
+        writer.close();
+    }
+
+    private static Polynomial fromMap(Map<Integer, Double> map) {
+        double[] coefficients = new double[map.size()];
+        int[] powers = new int[map.size()];
+        int i = 0;
+        for (Map.Entry<Integer, Double> entry : map.entrySet()) {
+            powers[i] = entry.getKey();
+            coefficients[i] = entry.getValue();
+            i++;
+        }
+        return new Polynomial(coefficients, powers);
     }
 
     public double evaluate(double x) {
         double value = 0;
-        for (int i = 0; i < this.coefficients.length; i++) {
-            value += this.coefficients[i] * Math.pow(x, i + 1);
+        for (int i = 0; i < this.powers.length; i++) {
+            value += this.coefficients[i] * Math.pow(x, this.powers[i]);
         }
         return value;
     }
@@ -33,5 +132,19 @@ public class Polynomial {
         return this.evaluate(x) == 0;
     }
 
+    @Override
+    public String toString() {
+        if (this.powers.length == 0) {
+            return "0";
+        }
 
+        StringBuilder string = new StringBuilder();
+        for (int i = 0; i < this.powers.length; i++) {
+            string.append(this.coefficients[i]).append("x").append(this.powers[i]);
+            if (i + 1 < this.powers.length && this.coefficients[i + 1] >= 0) {
+                string.append("+");
+            }
+        }
+        return string.toString();
+    }
 }
